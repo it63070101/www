c6e8f8233aa3977026ce7123b072225c15bc1e8c
suphafang <71120866+suphafang@users.noreply.github.com>
2020-10-30 15:06:14 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,23 +1,11 @@
+<!DOCTYPE html>
 <html>
-	<head>
-		<title>Hello world</title>
-	</head>
-	<body>
-		<?php
-		echo '
-		<h1>My First Heading</h1>
-		<p>My First Paragraph</p>
-		<img src="cat.png">
-		<br>
-		<a href="http://www.it.kmitl.ac.th">This is a Link</a>
-
-		<h2>The unordered list</h2>
-		<ul>
-			<li>Google</li>
-			<li>Yahoo</li>
-			<li>Amazon</li>
-		</ul>
-		';
-		?>
-	</body>
+<head>
+	<title>SuphaFang Webpage 2020</title>
+</head>
+<body>
+	<h1>Welcome To My Webpage</h1>
+	<h2>นายศุภชัย ชื่นสุขศรี (ฟ่าง)</h2>
+	
+</body>
 </html>
