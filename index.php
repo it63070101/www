29d0ee17a8c7101e42ab292fae41f9afb66d8f00
suphafang <71120866+suphafang@users.noreply.github.com>
2020-10-30 15:08:48 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 	<title>SuphaFang Webpage 2020</title>
 </head>
 <body>
-	<h1>Welcome To My Webpage</h1>
-	<h2>นายศุภชัย ชื่นสุขศรี (ฟ่าง)</h2>
-	
+	<h1 align="center">Welcome To My Webpage</h1>
+	<h2 align="center">นายศุภชัย ชื่นสุขศรี (ฟ่าง)</h2>
+	<img src="image-1.gif" width="400px" alt="This is me when I want to eat!">
 </body>
 </html>
