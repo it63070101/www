7138af45e5c38581141c8134754ed614b26572e4
suphafang <71120866+suphafang@users.noreply.github.com>
2020-11-14 02:39:19 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -27,6 +27,8 @@
 								    die('Failed to connect to MySQL: '.mysqli_connect_error());
 								}
 								$del = mysqli_query($conn, 'DELETE * FROM guestbook WHERE ID = '.$_GET['ID'].'');
+								if $del == True:
+									echo '<h2>DELETE SUCCESS</h2>';
 								mysqli_close($conn);
 							?>
 					</div>
