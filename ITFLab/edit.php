e841377e55eab3d66499f22564455933efee1d47
suphafang <71120866+suphafang@users.noreply.github.com>
2020-11-15 02:29:05 +0700

    ITFLab

---
+++ ITFLab/edit.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Edit | 13th ITF LAB</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+	<style type="text/css">
+		.card {
+			border-top: solid 5px #ffc107;
+		}
+	</style>
+</head>
+<body class="bg-light py-5">
+	<div class="container text-dark">
+		<div class="row">
+			<div class="col-12 col-lg-8 offset-lg-2">
+				<div class="card shadow">
+					<div class="card-body">
+						<div class="row">
+							<div class="col-6"><h1 class="text-monospace">Edit</h1></div>
+						</div>
+						<form action="insert.html">
+							<div class="form-group">
+								<label>Name</label>
+								<input type="text" name="" class="form-control" required>
+							</div>
+							<div class="form-group">
+								<label>Comment</label>
+								<textarea class="form-control" rows="5" required></textarea>
+							</div>
+							<div class="form-group">
+								<label>Link</label>
+								<input type="text" name="" class="form-control">
+							</div>
+							<div class="text-center">
+								<button type="submit" class="btn btn-sm btn-warning my-3 font-weight-bold">SAVE</button>
+								<a class="btn btn-sm btn-light my-3" href="index.html">CANCLE</a>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
