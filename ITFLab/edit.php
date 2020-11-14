3816d206639a28c566c21402daabdaa7d479a1e8
suphafang <71120866+suphafang@users.noreply.github.com>
2020-11-15 03:14:11 +0700

    Update edit.php

--- ITFLab/edit.php
+++ ITFLab/edit.php
@@ -41,9 +41,7 @@
 							</div>
 							<div class="form-group">
 								<label>Comment</label>
-								<textarea name="comment" class="form-control" rows="5" required>
-									<?php echo $data['Comment']; ?>
-								</textarea>
+								<textarea name="comment" class="form-control" rows="5" required><?php echo $data['Comment']; ?></textarea>
 							</div>
 							<div class="form-group">
 								<label>Link</label>
