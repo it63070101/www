77062972ba8d6b221cb7aa2de7c390977971ed90
suphafang <71120866+suphafang@users.noreply.github.com>
2020-11-15 03:09:59 +0700

    Update edit.php

--- ITFLab/edit.php
+++ ITFLab/edit.php
@@ -1,3 +1,17 @@
+<?php
+	$conn = mysqli_connect('suphafang.mysql.database.azure.com', 'suphafang@suphafang', 'Fang2545', 'ITFLab');
+
+	$id = $_GET['id'];
+
+	$sql = 'SELECT * FROM guestbook WHERE ID = '.$id.'';
+	$query = mysqli_query($conn, $sql);
+	if(!$query) {
+		header('Location: index.php');
+	}
+	else {
+		$data = mysqli_fetch_assoc($query);
+	}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -20,17 +34,18 @@
 							<div class="col-6"><h1 class="text-monospace">Edit</h1></div>
 						</div>
 						<form action="update.php" method="post">
+							<input type="text" name="id" value="<?php echo $data['ID']; ?>" class="form-control d-none" required>
 							<div class="form-group">
 								<label>Name</label>
-								<input type="text" name="" class="form-control" required>
+								<input type="text" name="name" value="<?php echo $data['Name']; ?>" class="form-control" required>
 							</div>
 							<div class="form-group">
 								<label>Comment</label>
-								<textarea class="form-control" rows="5" required></textarea>
+								<textarea name="comment" value="<?php echo $data['Comment']; ?>" class="form-control" rows="5" required></textarea>
 							</div>
 							<div class="form-group">
 								<label>Link</label>
-								<input type="text" name="" class="form-control">
+								<input type="text" name="link" value="<?php echo $data['Link']; ?>" class="form-control">
 							</div>
 							<div class="text-center">
 								<button type="submit" class="btn btn-sm btn-warning my-3 font-weight-bold">SAVE</button>
